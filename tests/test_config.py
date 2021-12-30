4d3fa00b9c20a1cff41ec6e927ea3ddc1a4aae07
Mayur Borkar <borkarm112@gmail.com>
2021-12-30 13:54:28 +0530

    pytest update

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,6 +1,15 @@
 # In that File Always Define Function With test Name
+import pytest
+
+
+class NotInRange(Exception):
+    def __init__(self, message="Value Not In Range"):
+        self.message = message
+        super().__init__(self.message)
+
 
 def test_generic():
-    a = 10
-    b = 10
-    assert a == b
+    a = 5
+    with pytest.raises(NotInRange):
+        if a not in range(10, 6):
+            raise NotInRange
