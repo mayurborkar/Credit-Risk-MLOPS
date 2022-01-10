e896332daa3a3677c0d92b5c73ef294e045c0740
Mayur Borkar <borkarm112@gmail.com>
2022-01-10 13:56:44 +0530

    procfile update

--- app.py
+++ app.py
@@ -1,34 +1,25 @@
-from flask import Flask, request, render_template
-from wsgiref import simple_server
-import joblib
-import yaml
+from prediction_service import prediction
+from flask import Flask, render_template, request, jsonify
 import os
+import numpy as np
 
-params_path = "params.yaml"
-templates = os.path.join("webapp", "templates")
+webapp_root = "webapp"
 
+static_dir = os.path.join(webapp_root, "static")
+template_dir = os.path.join(webapp_root, "templates")
 
-def read_params(config_path=params_path):
-    with open(config_path) as yaml_file:
-        config = yaml.safe_load(yaml_file)
-    return config
-
-
-config = read_params(params_path)
-model_dir_path = config["webapp_model_dir"]
-model = joblib.load(model_dir_path)
-
-app = Flask(__name__, template_folder=templates)
+app = Flask(__name__, static_folder=static_dir,template_folder=template_dir)
 
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
     """
-    Method_Name: home
+    :Method_Name: home
     :DESC: This Will Return The Home Page
     :param: None
     :return: index.html
     """
+
     try:
         return render_template('index.html')
 
@@ -36,70 +27,32 @@ def home():
         raise Exception(f'(Home)- Something Went Wrong With The Method \n' + str(e))
 
 
-@app.route("/predict", methods=['POST'])
-def predict():
-    """
-    Method_Name: predict
-    :DESC: This Will Return The Credit Risk Is Bad or Good
-    :param: None
-    :return: The Risk Is Bad or Good
-    """
-    try:
-        if request.method == "POST":
-            try:
-                status = int(request.form['status'])
+@app.route("/predict", methods=["GET", "POST"])
+def index():
 
-                duration = int(request.form['duration'])
-
-                credit_history = int(request.form['credit_history'])
-
-                purpose = int(request.form['purpose'])
-
-                amount = int(request.form['amount'])
-
-                savings = int(request.form['savings'])
-
-                employment_duration = int(request.form['employment_duration'])
-
-                personal_status_sex = int(request.form['personal_status_sex'])
-
-                installment_rate = int(request.form['installment_rate'])
-
-                present_residence = int(request.form['present_residence'])
-
-                property = int(request.form['property'])
-
-                age = int(request.form['age'])
-
-                number_credits = int(request.form['number_credits'])
-
-                telephone = int(request.form['telephone'])
-
-                value = model.predict([[status, duration, credit_history, purpose, amount, savings,
-                                        employment_duration, personal_status_sex, installment_rate,
-                                        present_residence, property, age, number_credits, telephone]])
-
-                if value == 0:
+    if request.method == "POST":
+        try:
+            if request.form:
+                dict_req = dict(request.form)
+                response = prediction.form_response(dict_req)
+                if response == 0:
                     label = 'Bad'
                 else:
                     label = 'Good'
-
                 return render_template('result.html', prediction_text=" The Credit Risk Is {}".format(label))
+            elif request.json:
+                response = prediction.api_response(request.json)
+                return jsonify(response)
 
-            except Exception as e:
-                raise Exception(f'(Predict)- Something Went Wrong With The Method \n' + str(e))
+        except Exception as e:
+            print(e)
+            error = {"error": "Something went wrong!! Try again later!"}
+            error = {"error": e}
 
-        else:
-            return render_template('index.html')
-
-    except Exception as e:
-        raise Exception(f'(Predict)- Something Went Wrong With The Method \n' + str(e))
+            return render_template("404.html", error=error)
+    else:
+        return render_template("index.html")
 
 
-port = int(os.getenv("PORT", 5000))
 if __name__ == "__main__":
-    host = '0.0.0.0'
-    # port = 5000
-    httpd = simple_server.make_server(host, port, app)
-    # print("Serving on %s %d" % (host, port))
-    httpd.serve_forever()
+    app.run(host='0.0.0.0', port=5000, debug=True)
--- src/get_data.py
+++ src/get_data.py
@@ -10,6 +10,7 @@ import argparse
 def read_params(config_path):
     with open(config_path) as yaml_file:
         config = yaml.safe_load(yaml_file)
+        #print(config)
     return config
 
 
