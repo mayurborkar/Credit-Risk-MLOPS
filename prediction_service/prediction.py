c25f8c836342cc168578a05d310c4bac8cb1785b
Mayur Borkar <borkarm112@gmail.com>
2022-01-10 21:07:54 +0530

    requirements file update

--- prediction_service/prediction.py
+++ prediction_service/prediction.py
@@ -82,15 +82,13 @@ def api_response(dict_request):
             return response
 
     except NotInRange as e:
-        response = {"the_exected_range": get_schema(), "response": str(e)}
+        response = {"the_expected_range": get_schema(), "response": str(e)}
         return response
 
     except NotInCols as e:
-        response = {"the_exected_cols": get_schema().keys(), "response": str(e)}
+        response = {"the_expected_cols": get_schema().keys(), "response": str(e)}
         return response
-
 
     except Exception as e:
         response = {"response": str(e)}
         return response
-
